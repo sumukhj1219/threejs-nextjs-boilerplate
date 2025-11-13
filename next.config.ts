4125e77aa177350c9251ef03c043f44b1ea973c7
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-11-13 18:20:58 +0530

    added config

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,13 @@ const nextConfig: NextConfig = {
     });
     return config;
   },
+
+  typescript:{
+    ignoreBuildErrors:true
+  },
+  eslint:{
+    ignoreDuringBuilds:true
+  }
 };
 
 export default nextConfig;
